c7d2600d21a153cd2224b9e7ba3be2b9c27b50b2
maciolem <marcin,maciolek@roche.com>
2015-07-13 14:57:11 +0200

    1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justTryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the justTryApp
+ */
+angular.module('justTryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
